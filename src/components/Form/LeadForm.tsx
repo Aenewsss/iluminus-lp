a337846de7beee6bb03c4a5140a54d27e8d48f9a
aenewsss <aenamartinelli@gmail.com>
2023-10-03 16:05:03 -0300

    new form responsive

--- src/components/Form/LeadForm.tsx
+++ src/components/Form/LeadForm.tsx
@@ -4,11 +4,11 @@ const LeadForm = () => {
     return (
         <form>
             <div className="mb-3 d-flex gap-3 flex-md-nowrap flex-wrap">
-                <input required type="text" className="form-control pt-3 pb-3 fs-4" placeholder="Nome Completo" />
-                <input required type="text" className="form-control pt-3 pb-3 fs-4" placeholder="Telefone" />
+                <input required type="text" className="form-control pt-2 pb-2 pt-md-3 pb-md-3 fs-4" placeholder="Nome Completo" />
+                <input required type="text" className="form-control pt-2 pb-2 pt-md-3 pb-md-3 fs-4" placeholder="Telefone" />
             </div>
             <div className="mb-3">
-                <input required type="email" className="form-control pt-3 pb-3 fs-4" placeholder="Digite seu melhor e-mail" />
+                <input required type="email" className="form-control pt-2 pb-2 pt-md-3 pb-md-3 fs-4" placeholder="Digite seu melhor e-mail" />
             </div>
             <Save />
         </form>
