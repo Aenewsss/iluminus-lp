54bdb9159ef122f8cc040fec53c278de15c8ebf8
aenewsss <aenamartinelli@gmail.com>
2023-10-18 22:17:46 -0300

    insert facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from "next/script"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,28 @@ export default function RootLayout({
         <link rel="icon" href="favicon.svg" type="image/png" />
         <meta name="theme-color" content="#000" />
         <meta name="facebook-domain-verification" content="vxts5yasipoxrikgnmx65iqxj197nz" />
+
+        {/* Meta Pixel Code  */}
+        <Script id="fb-pixel" 
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2005714673161741');
+              fbq('track', 'PageView');
+          `
+          }}
+        />
+        <noscript>
+          {/* @ts-ignore */}
+          <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2005714673161741&ev=PageView&noscript=1" /></noscript>
+        {/* End Meta Pixel Code  */}
       </head>
 
       <body className={inter.className}>{children}</body>
