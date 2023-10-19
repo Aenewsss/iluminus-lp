8ae7f6ee8f4f469cef99641fb24054573030ce74
aenewsss <aenamartinelli@gmail.com>
2023-10-19 09:57:48 -0300

    change pixel lead

--- src/components/PixelMeta/PixelObrigado.tsx
+++ src/components/PixelMeta/PixelObrigado.tsx
@@ -1,11 +1,17 @@
+'use client'
+
+import { usePathname } from "next/navigation";
 import Script from "next/script"
+import { useEffect } from "react";
 
 let time = 0
 
 
 const PixelObrigado = () => {
 
-    if (time == 0) {
+    const pathname = usePathname()
+
+    if (time == 0 && pathname == '/obrigado') {
         time = 1
         return (
             <>
